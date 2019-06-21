ca526d303d97af83900fec40229967ea3da8a2e1
Anton <belitskeya@gmail.com>
2019-06-21 09:37:30 +0300

    add image_sizes variables

--- save_image/background_photo/models.py
+++ save_image/background_photo/models.py
@@ -3,14 +3,13 @@ from django.db import models
 from smartfields import fields
 from smartfields.dependencies import FileDependency
 from smartfields.processors import ImageProcessor
+from .image_sizes import CONSUMER_BACKGROUND_WIDTH, CONSUMER_BACKGROUND_HEIGHT
 
 
 class Mymodel(models.Model):
-    width = 300
-    height = 300
     resize_photo = fields.ImageField(upload_to='story1399', null=True, blank=True, dependencies=[
         FileDependency(attname='original_photo', processor=ImageProcessor(
-            format='JPEG', scale={'max_width': width, 'max_height': height}))
+            format='JPEG', scale={'max_width': CONSUMER_BACKGROUND_WIDTH, 'max_height': CONSUMER_BACKGROUND_HEIGHT}))
     ])
     original_photo = models.ImageField(upload_to='story1399', null=True, blank=True)
 
